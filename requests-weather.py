210580eb5bc913b3bf4bab2ec65d1029ef6a2a60
John Hudson <drinkfalconpunch@gmail.com>
2018-06-04 09:59:24 -0500

    adding darksky functionality

--- requests-weather.py
+++ requests-weather.py
@@ -1,7 +1,36 @@
 import requests
 import config
+import datetime
 
-header = {token: config.api_key}
+DARKSKY_FORECAST_URL = 'https://api.darksky.net/forecast/'
 
-r = requests.get('https://query.yahooapis.com/v1/public/yql?')
-print(r.url)
+header = {'token': config.api_key_darksky}
+
+def get_darksky_data(api_key, lat, long):
+    url = DARKSKY_FORECAST_URL + str(header['token']) + '/' + str(lat) + '/' + str(long)
+    print(url)
+    r = requests.get(DARKSKY_FORECAST_URL + str(header['token']) + '/' + str(lat) + '/' + str(long))
+
+    return r.json()
+
+def show_hourly_forecast(api_key, lat, long):
+    response_data = get_darksky_data(api_key, lat, long)
+    hourly_forecast = dict()
+    hourly_data = response_data['hourly']['data']
+
+    for data in hourly_data:
+        hourly_forecast[datetime.datetime.fromtimestamp(data['time']).strftime('%Y-%m-%d %H:%M:%S')] = data['temperature']
+
+    return hourly_forecast
+
+def show_minutely_forecast(api_key, lat, long):
+    response_data = get_darksky_data(api_key, lat, long)
+    minutely_forecast = dict()
+    minutely_data = response_data['minutely']['data']
+
+    for data in mintely_data:
+        minutely_forecast[datetime.datetime.fromtimestamp(data['time']).strftime('%Y-%m-%d %H:%M:%S')] = data['temperature']
+
+    return minutely_forecast
+
+print(show_hourly_forecast(header['token'], +35.53860, -97.90141))
