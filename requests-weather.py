9c46ace21890bde4166ce2e5a582f9155aa86cc3
John Hudson <drinkfalconpunch@gmail.com>
2018-06-04 11:56:45 -0500

    made api calls into a class

--- requests-weather.py
+++ requests-weather.py
@@ -2,51 +2,56 @@ import requests
 import config
 import datetime
 
-DARKSKY_FORECAST_URL = 'https://api.darksky.net/forecast/'
-
 api_key_darksky = config.api_key_darksky
 
 def unixtime_to_readable(unixtime):
     return datetime.datetime.fromtimestamp(unixtime).strftime('%Y-%m-%d %H:%M:%S')
 
-def get_darksky_data(api_key, lat, long):
-    url = DARKSKY_FORECAST_URL + str(api_key) + '/' + str(lat) + ',' + str(long)
-    r = requests.get(url)
+class Darksky(object):
+    DARKSKY_FORECAST_URL = 'https://api.darksky.net/forecast/'
 
-    return r.json()
+    def __init__(self, api_key=None, lat=None, long=None, **kwargs):
+        self.api_key = api_key
+        self.lat = lat
+        self.long = long
+        self.data = None
+        self.time = None
+        if api_key is not None:
+            self.get_darksky_data()
 
-def get_hourly_forecast(api_key, lat, long):
-    response_data = get_darksky_data(api_key, lat, long)
-    hourly_forecast = dict()
-    hourly_data = response_data['hourly']['data']
-    for data in hourly_data:
-        data['time'] = unixtime_to_readable(data['time'])
-#    for data in hourly_data:
-#        hourly_forecast[datetime.datetime.fromtimestamp(data['time']).strftime('%Y-%m-%d %H:%M:%S')] = data['temperature']
 
-    return hourly_data
+    def get_darksky_data(self):
+        url = Darksky.DARKSKY_FORECAST_URL + str(self.api_key) + '/' + str(self.lat) + ',' + str(self.long)
+        self.data = requests.get(url).json()
+#        for key, value in dictionary.items():
+#            if isinstance(value, dict):
+#                iterate(value)
+#                continue
+        self.time = datetime.datetime.now()
 
-def get_minutely_forecast(api_key, lat, long):
-    response_data = get_darksky_data(api_key, lat, long)
-    minutely_forecast = dict()
-    minutely_data = response_data['minutely']['data']
 
-    for data in minutely_data:
-        data['time'] = unixtime_to_readable(data['time']) 
-#    for data in minutely_data:
-#        minutely_forecast[datetime.datetime.fromtimestamp(data['time']).strftime('%Y-%m-%d %H:%M:%S')] = data['temperature']
+    def get_hourly_forecast(self):
+        hourly_data = self.data['hourly']['data']
+        for data in hourly_data:
+            data['time'] = unixtime_to_readable(data['time'])
 
-    return minutely_data
+        return hourly_data
 
-def get_current_weather(api_key, lat, long):
-    response_data = get_darksky_data(api_key, lat, long)
-    current_weather = response_data['currently']
+    def get_minutely_forecast(self):
+        minutely_data = self.data['minutely']['data']
 
-    return current_weather
+        for data in minutely_data:
+            data['time'] = unixtime_to_readable(data['time']) 
 
+        return minutely_data
+
+    def get_current_weather(self):
+        current_weather = self.data['currently']
+
+        return current_weather
 
 if __name__=='__main__':
     lat = +35.53860
     long = -97.90141
-    darksky_data = get_darksky_data(api_key_darksky, lat, long)
-    print(get_hourly_forecast(api_key_darksky, lat, long))
+    darksky_data = Darksky(api_key_darksky, lat, long)
+    print(darksky_data.get_current_weather())
