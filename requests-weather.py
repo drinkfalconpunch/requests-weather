aa488939f4bbcd86269e6c75e67ba2fbb9789895
John Hudson <drinkfalconpunch@gmail.com>
2018-06-04 10:28:18 -0500

    minute data does not have temps

--- requests-weather.py
+++ requests-weather.py
@@ -4,33 +4,40 @@ import datetime
 
 DARKSKY_FORECAST_URL = 'https://api.darksky.net/forecast/'
 
-header = {'token': config.api_key_darksky}
+api_key_darksky = config.api_key_darksky
 
 def get_darksky_data(api_key, lat, long):
-    url = DARKSKY_FORECAST_URL + str(header['token']) + '/' + str(lat) + '/' + str(long)
-    print(url)
-    r = requests.get(DARKSKY_FORECAST_URL + str(header['token']) + '/' + str(lat) + '/' + str(long))
+    url = DARKSKY_FORECAST_URL + str(api_key) + '/' + str(lat) + ',' + str(long)
+    r = requests.get(url)
 
     return r.json()
 
-def show_hourly_forecast(api_key, lat, long):
+def get_hourly_forecast(api_key, lat, long):
     response_data = get_darksky_data(api_key, lat, long)
     hourly_forecast = dict()
     hourly_data = response_data['hourly']['data']
 
-    for data in hourly_data:
-        hourly_forecast[datetime.datetime.fromtimestamp(data['time']).strftime('%Y-%m-%d %H:%M:%S')] = data['temperature']
+#    for data in hourly_data:
+#        hourly_forecast[datetime.datetime.fromtimestamp(data['time']).strftime('%Y-%m-%d %H:%M:%S')] = data['temperature']
 
-    return hourly_forecast
+    return hourly_data
 
-def show_minutely_forecast(api_key, lat, long):
+def get_minutely_forecast(api_key, lat, long):
     response_data = get_darksky_data(api_key, lat, long)
     minutely_forecast = dict()
     minutely_data = response_data['minutely']['data']
 
-    for data in mintely_data:
-        minutely_forecast[datetime.datetime.fromtimestamp(data['time']).strftime('%Y-%m-%d %H:%M:%S')] = data['temperature']
+#    for data in minutely_data:
+#        minutely_forecast[datetime.datetime.fromtimestamp(data['time']).strftime('%Y-%m-%d %H:%M:%S')] = data['temperature']
 
-    return minutely_forecast
+    return minutely_data
 
-print(show_hourly_forecast(header['token'], +35.53860, -97.90141))
+def get_current_weather(api_key, lat, long):
+    response_data = get_darksky_data(api_key, lat, long)
+    current_weather = response_data['currently']
+
+    return current_weather
+
+
+if __name__=='__main__':
+    print(get_minutely_forecast(api_key_darksky, +35.53860, -97.90141))
