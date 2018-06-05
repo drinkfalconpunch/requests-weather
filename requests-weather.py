e378fff09f35edae8f66c288568e84c51359de1b
John Hudson <drinkfalconpunch@gmail.com>
2018-06-05 08:59:59 -0500

    can tranverse the dictionary. still returns None

--- requests-weather.py
+++ requests-weather.py
@@ -7,35 +7,35 @@ api_key_darksky = config.api_key_darksky
 def unixtime_to_readable(unixtime):
     return datetime.datetime.fromtimestamp(unixtime).strftime('%Y-%m-%d %H:%M:%S')
 
-def iterate_dict(src, search_key=None, replace_func=None, in_place=False):
+def iterate_dict(src, search_key=None, replace_func=None):
     if isinstance(src, dict):
         for key, value in src.items():
             if key==search_key and replace_func:
-                value = replace_func(value)
+                src[key] = replace_func(value)
             else:
-                value = iterate_dict(value, search_key=search_key, replace_func=replace_func)
+                iterate_dict(value, search_key=search_key, replace_func=replace_func)
     elif isinstance(src, list):
         for data in src:
-            data = iterate_dict(data, search_key=search_key, replace_func=replace_func)
+            iterate_dict(data, search_key=search_key, replace_func=replace_func)
     else:
-        print(src)
+        return src
 
 class Darksky(object):
     darksky_url = 'https://api.darksky.net/forecast/'
 
-    def __init__(self, api_key=None, lat=None, long=None, **kwargs):
+    def __init__(self, api_key=None, latitude=None, longitude=None, **kwargs):
         self.api_key = api_key
-        self.lat = lat
-        self.long = long
+        self.latitude = latitude
+        self.longitude = longitude
         self.data = None
         self.time = None
-        if api_key is not None:
+        if api_key:
             self.get_darksky_data()
         print(self.data)
 
 
     def get_darksky_data(self):
-        url = Darksky.darksky_url + str(self.api_key) + '/' + str(self.lat) + ',' + str(self.long)
+        url = Darksky.darksky_url + str(self.api_key) + '/' + str(self.latitude) + ',' + str(self.longitude)
         self.data = requests.get(url).json()
         self.time = datetime.datetime.now()
         self.data = iterate_dict(self.data, search_key='time', replace_func=unixtime_to_readable)
@@ -60,7 +60,7 @@ class Darksky(object):
         return self.data['currently']
 
 if __name__=='__main__':
-    lat = +35.53860
-    long = -97.90141
-    darksky_data = Darksky(api_key_darksky, lat, long)
+    latitude = +35.53860
+    longitude = -97.90141
+    darksky_data = Darksky(api_key_darksky, latitude, longitude)
     print(darksky_data.get_hourly_forecast())
