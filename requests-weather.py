554616d12ac1bdf0b89608e1927cb34120380ab0
John Hudson <drinkfalconpunch@gmail.com>
2018-06-05 09:14:01 -0500

    got it working. need to fix time update now

--- requests-weather.py
+++ requests-weather.py
@@ -7,18 +7,17 @@ api_key_darksky = config.api_key_darksky
 def unixtime_to_readable(unixtime):
     return datetime.datetime.fromtimestamp(unixtime).strftime('%Y-%m-%d %H:%M:%S')
 
-def iterate_dict(src, search_key=None, replace_func=None):
-    if isinstance(src, dict):
-        for key, value in src.items():
-            if key==search_key and replace_func:
-                src[key] = replace_func(value)
-            else:
-                iterate_dict(value, search_key=search_key, replace_func=replace_func)
-    elif isinstance(src, list):
-        for data in src:
-            iterate_dict(data, search_key=search_key, replace_func=replace_func)
-    else:
-        return src
+def iterate_dict(src: dict, search_key=None, replace_func=None):
+    for key, value in src.items():
+        if isinstance(value, dict):
+            iterate_dict(value, search_key=search_key, replace_func=replace_func)
+        elif isinstance(value, list):
+            for data in value:
+                iterate_dict(data, search_key=search_key, replace_func=replace_func)
+        elif key==search_key and replace_func:
+            src[key] = replace_func(value)
+        else:
+            return src
 
 class Darksky(object):
     darksky_url = 'https://api.darksky.net/forecast/'
@@ -31,8 +30,6 @@ class Darksky(object):
         self.time = None
         if api_key:
             self.get_darksky_data()
-        print(self.data)
-
 
     def get_darksky_data(self):
         url = Darksky.darksky_url + str(self.api_key) + '/' + str(self.latitude) + ',' + str(self.longitude)
@@ -40,21 +37,11 @@ class Darksky(object):
         self.time = datetime.datetime.now()
         self.data = iterate_dict(self.data, search_key='time', replace_func=unixtime_to_readable)
 
-
     def get_hourly_forecast(self):
-        hourly_data = self.data['hourly']['data']
-#        for data in hourly_data:
-#            data['time'] = unixtime_to_readable(data['time'])
-
-        return hourly_data
+        return self.data['hourly']['data']
 
     def get_minutely_forecast(self):
-        minutely_data = self.data['minutely']['data']
-
-        for data in minutely_data:
-            data['time'] = unixtime_to_readable(data['time']) 
-
-        return minutely_data
+        return self.data['minutely']['data']
 
     def get_current_weather(self):
         return self.data['currently']
